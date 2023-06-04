8372a0c4059c45aef4d5033f195e8060c9f2b163
Rahul Thakur <36036600+riskyRahul@users.noreply.github.com>
2023-06-04 14:55:54 +0530

    first commit

---
+++ lib/utils/helperfunctions.dart
@@ -0,0 +1,24 @@
+import 'package:web3dart/crypto.dart';
+
+String truncateString(String text, int front, int end) {
+  int size = front + end;
+
+  if (text.length > size) {
+    String finalString =
+        "${text.substring(0, front)}...${text.substring(text.length - end)}";
+    return finalString;
+  }
+
+  return text;
+}
+
+String generateSessionMessage(String accountAddress) {
+  String message =
+      'Hello $accountAddress, welcome to our app. By signing this message you agree to learn and have fun with blockchain';
+  print(message);
+  
+  var hash = keccakUtf8(message);
+  final hashString = '0x${bytesToHex(hash).toString()}';
+
+  return hashString;
+}
